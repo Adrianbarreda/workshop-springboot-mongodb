0280a03d429543d948dbe456bd6fe5dfc36a8757
Adrianbarreda <adrianbarreda1052@gmail.com>
2022-06-02 20:54:53 -0400

    Obtendo um post por id

---
+++ src/main/java/com/adrianbarreda/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.adrianbarreda.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adrianbarreda.workshopmongo.domain.Post;
+import com.adrianbarreda.workshopmongo.repository.PostRepository;
+import com.adrianbarreda.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	/*
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}*/
+}
